828f4d867fad9ae318e20a9ed979244e44b87db2
iurifulgidi <iurifulgidi@hotmail.com>
2019-11-14 22:13:53 +0100

    miglioramenti grafici

--- Merendero_F/Merendero_F/Merendero_F/MainPage.xaml.cs
+++ Merendero_F/Merendero_F/Merendero_F/MainPage.xaml.cs
@@ -27,14 +27,14 @@ namespace Merendero_F
         {
             merende = new ObservableCollection<Merenda>
             {
-                new Merenda { Name = "Speciale", Description = "Pizza farcita con pomodoro, prosciutto cotto e mozzarella",ImgUrl="https://www.ifood.it/wp-content/uploads/2017/10/focaccia-laica-dettaglio-1160x773.jpg" , Cost =1.50, Quantity=0 },
-                new Merenda { Name = "Ciambella", Description = "Per uno spuntino veloce e dolce", ImgUrl ="https://images.pexels.com/photos/41300/berliner-breakfast-bun-cake-41300.jpeg", Cost =1, Quantity=0 },
-                new Merenda { Name = "Lemon Soda", Description = "Classica Lemon Soda", ImgUrl = "http://www.italianherkut.fi/970-large_default/lemonsoda-33cl-campari.jpg" , Cost =1, Quantity=0 },
-                new Merenda { Name = "Coca Cola", Description = "Classica Coca Cola", ImgUrl = "https://www.cicalia.com/it/img/imgproducts/1525/l_1525.jpg" , Cost =1, Quantity=0 },
-                new Merenda { Name = "Fanta", Description = "Un succo al'arancia Fantastico", ImgUrl = "https://www.cicalia.com/it/img/imgproducts/44234/l_44234.jpg" , Cost =1, Quantity=0 },
-                new Merenda { Name = "Focaccia al salame", Description = "Una bella focaccia al salame", ImgUrl = "https://www.lineasnack.it/wp-content/uploads/2017/07/focaccia-salame.jpg" , Cost =1.50, Quantity=0 },
-                new Merenda { Name = "Focaccia al crudo", Description = "Una bella focaccia con il prosciutto crudo", ImgUrl = "https://www.lineasnack.it/wp-content/uploads/2017/07/focaccia-crudo.jpg" , Cost =1.50, Quantity=0 },
-                new Merenda { Name = "Focaccia al cotto", Description = "Una bella focaccia con il prosciutto cotto", ImgUrl = "https://www.lineasnack.it/wp-content/uploads/2017/07/cotto.jpg" , Cost =1.50, Quantity=0 }
+                new Merenda { Name = "Speciale", Description = "Pizza farcita con pomodoro, prosciutto cotto e mozzarella",ImgUrl="https://cdn1.imggmi.com/uploads/2019/11/14/c167485a5abb97ed4128bf6b9fc20fb4-full.jpg" , Cost =1.50, Quantity=0 },
+                new Merenda { Name = "Focaccia al salame", Description = "Una bella focaccia al salame", ImgUrl = "https://cdn1.imggmi.com/uploads/2019/11/15/2bcb4bd808b417c8e80110617b9deca6-full.jpg" , Cost =1.50, Quantity=0 },
+                new Merenda { Name = "Focaccia al crudo", Description = "Una bella focaccia con il prosciutto crudo", ImgUrl = "https://cdn1.imggmi.com/uploads/2019/11/15/7aa36b468d021317a7f29f95fbba6c98-full.jpg" , Cost =1.50, Quantity=0 },
+                new Merenda { Name = "Focaccia al cotto", Description = "Una bella focaccia con il prosciutto cotto", ImgUrl = "https://cdn1.imggmi.com/uploads/2019/11/14/b3ac928a8e5b0442b44e639771d0a815-full.jpg" , Cost =1.50, Quantity=0 },
+                new Merenda { Name = "Ciambella", Description = "Per uno spuntino veloce e dolce", ImgUrl ="https://cdn1.imggmi.com/uploads/2019/11/14/24ac822e21945f2a1c374e5ca9706aed-full.jpg", Cost =1, Quantity=0 },
+                new Merenda { Name = "Lemon Soda", Description = "Classica Lemon Soda", ImgUrl = "https://cdn1.imggmi.com/uploads/2019/11/15/0490e119a18b50d3d6574f163057281c-full.jpg" , Cost =1, Quantity=0 },
+                new Merenda { Name = "Coca Cola", Description = "Classica Coca Cola", ImgUrl = "https://cdn1.imggmi.com/uploads/2019/11/14/a643791d83b97ae95f83af47c85972df-full.jpg" , Cost =1, Quantity=0 },
+                new Merenda { Name = "Fanta", Description = "Un succo al'arancia Fantastico", ImgUrl = "https://cdn1.imggmi.com/uploads/2019/11/15/4534388405873fcc1e4153bb1b8b5832-full.jpg" , Cost =1, Quantity=0 },
                 //new item template
                 //new Merenda { Name = "", Description = "", ImgUrl = "" , Cost =1, Quantity=0 },
             };
