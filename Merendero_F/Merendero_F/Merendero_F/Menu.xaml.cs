35ca53315cd0e30677c7bd050152b3feb4221792
iurifulgidi <iurifulgidi@hotmail.com>
2019-12-17 16:55:02 +0100

    modifica di alcune immagini

--- Merendero_F/Merendero_F/Merendero_F/Menu.xaml.cs
+++ Merendero_F/Merendero_F/Merendero_F/Menu.xaml.cs
@@ -28,33 +28,43 @@ namespace Merendero_F
         {
             salato = new ObservableCollection<Merenda>
             {
+                 //immagine royality free
+                new Merenda { Name = "Pizza", Description = "Pizza margherita al taglio", ImgUrl = "pizza.png" , Cost =1, Quantity=0 },
+
+                //immagine illegale
                 new Merenda { Name = "Speciale", Description = "Pizza farcita con pomodoro, prosciutto cotto e mozzarella",ImgUrl="speciale.png" , Cost =1.50 , Quantity=0 },
                 new Merenda { Name = "Focaccia al salame", Description = "Focaccia farcita al salame", ImgUrl = "salame.png" , Cost =1.50, Quantity=0 },
                 new Merenda { Name = "Focaccia al crudo", Description = "Focaccia farcita con il prosciutto crudo", ImgUrl = "crudo.png" , Cost =1.50 , Quantity=0 },
-                new Merenda { Name = "Focaccia al cotto", Description = "Focaccia farcita con il prosciutto cotto", ImgUrl = "cotto.png" , Cost =1.50 , Quantity=0 }, 
-                new Merenda { Name = "Pizza", Description = "Pizza margherita al taglio", ImgUrl = "pizza.png" , Cost =1, Quantity=0 },
+                new Merenda { Name = "Focaccia al cotto", Description = "Focaccia farcita con il prosciutto cotto", ImgUrl = "cotto.png" , Cost =1.50 , Quantity=0 }
+                
             };
             dolce = new ObservableCollection<Merenda>
             {
+                //immagine royality free
                 new Merenda { Name = "Ciambella", Description = "ciambella fritta con zucchero a velo", ImgUrl = "ciambella.png" , Cost =1 , Quantity=0 },
+
+                //immagine illegale
                 new Merenda { Name = "Cornetto vuoto", Description = "Dolce cornetto senza ripieno", ImgUrl = "cornetto_vuoto.png" , Cost =1, Quantity=0 },
                 new Merenda { Name = "Cornetto alla nutella", Description = "Dolce cornetto ripieno di buonissima nutella", ImgUrl = "cornetto_cioccolato.png" , Cost =1, Quantity=0 },
                 new Merenda { Name = "Cornetto all'albicocca", Description = "Dolce cornetto ripieno di marmellata all' albicocca", ImgUrl = "cornetto_albicocca.png" , Cost =1, Quantity=0 },
                 new Merenda { Name = "Cornetto alla crema", Description = "Dolce cornetto ripieno alla crema", ImgUrl = "cornetto_crema.jpg" , Cost =1, Quantity=0 },
                 new Merenda { Name = "Bombolone alla crema", Description = "Bombolone fritto ripieno di crema", ImgUrl = "bombolone_crema.png" , Cost =1, Quantity=0 },
-                new Merenda { Name = "Bombolone alla nutella", Description = "Bombolone fritto ripieno di nutella", ImgUrl = "bombolone_nutella.png" , Cost =1, Quantity=0 },
+                new Merenda { Name = "Bombolone alla nutella", Description = "Bombolone fritto ripieno di nutella", ImgUrl = "bombolone_nutella.png" , Cost =1, Quantity=0 }
             };
             bevande = new ObservableCollection<Merenda>
             {
-                new Merenda { Name = "Lemon Soda", Description = "Bevanda gassata al gusto di limone", ImgUrl = "lemonsoda.png" , Cost =1 , Quantity=0 },
-                new Merenda { Name = "Oran Soda", Description = "Bevanda gassata al gusto di arancia", ImgUrl = "oransoda.png" , Cost =1, Quantity=0 },
+                //immagine royality free
+                new Merenda { Name = "Acqua naturale", Description = "Bottiglietta da 0,5L di acqua nuaturale ", ImgUrl = "acqua_naturale.png" , Cost =0.5, Quantity=0 },
+                new Merenda { Name = "Acqua Frizzante", Description = "Bottiglietta da 0,5L di acqua frizzante", ImgUrl = "acqua_frizzante.png" , Cost =0.5, Quantity=0 },
                 new Merenda { Name = "Coca Cola", Description = "Taste the feeling", ImgUrl = "coca.png" , Cost =1, Quantity=0 },
                 new Merenda { Name = "Fanta", Description = "Un succo al'arancia Fantastico", ImgUrl = "fanta.png" , Cost =1 , Quantity=0 },
                 new Merenda { Name = "Sprite", Description = "Sprite", ImgUrl = "sprite.png" , Cost =1, Quantity=0 },
+
+                 //immagine illegale
                 new Merenda { Name = "Etstathè al Limone", Description = "Brick di thè infuso alla pesca", ImgUrl = "estathe_limone.png" , Cost =1, Quantity=0 },
                 new Merenda { Name = "Etstathè alla pesca", Description = "Brick di thè infuso al limone", ImgUrl = "etathe_pesca.png" , Cost =1, Quantity=0 },
-                new Merenda { Name = "Acqua naturale", Description = "Bottiglietta da 0,5L di acqua nuaturale ", ImgUrl = "acqua_naturale.png" , Cost =0.5, Quantity=0 },
-                new Merenda { Name = "Acqua Frizzante", Description = "Bottiglietta da 0,5L di acqua frizzante", ImgUrl = "acqua_frizzante.png" , Cost =0.5, Quantity=0 }
+                new Merenda { Name = "Lemon Soda", Description = "Bevanda gassata al gusto di limone", ImgUrl = "lemonsoda.png" , Cost =1 , Quantity=0 },
+                new Merenda { Name = "Oran Soda", Description = "Bevanda gassata al gusto di arancia", ImgUrl = "oransoda.png" , Cost =1, Quantity=0 }
             };
             //new Merenda { Name = "", Description = "", ImgUrl = "" , Cost =1, Quantity=0 },
         }
