841e1279371f434caa23abc757ff6878b515c5d9
iurifulgidi <iurifulgidi@hotmail.com>
2019-12-18 18:10:44 +0100

    cambio nome app, immagini

--- Merendero_F/Merendero_F/Merendero_F.Android/MainActivity.cs
+++ Merendero_F/Merendero_F/Merendero_F.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Merendero_F.Droid
 {
-    [Activity(Label = "Merendero_F", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Let's Eat", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
